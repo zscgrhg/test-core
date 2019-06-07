f6997b5a4172bb7ebba2d35fbd51dd423f9653b5
lwk <zscgrhg@qq.com>
2019-06-08 01:24:28 +0800

    555

--- support-api/src/main/java/com/zte/crm/prm/supportapi/HelloContract.java
+++ support-api/src/main/java/com/zte/crm/prm/supportapi/HelloContract.java
@@ -1,12 +1,11 @@
 package com.zte.crm.prm.supportapi;
 
 import com.zte.crm.prm.anno.RemoteServiceContract;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "hello", url = "http://localhost:8881")
-@RemoteServiceContract(name = "zte-crm-prm-common")
-public interface HelloContract  {
+
+@RemoteServiceContract(name = "zte-crm-prm-common",url = "http://localhost:8881")
+public interface HelloContract {
     @RequestMapping("/hello")
     String hello();
 }
