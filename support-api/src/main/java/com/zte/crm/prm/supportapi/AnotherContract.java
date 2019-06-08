f08bd1089e9733e09f219882804f6a0b93733c06
lwk <zscgrhg@qq.com>
2019-06-08 13:17:30 +0800

    555

--- support-api/src/main/java/com/zte/crm/prm/supportapi/AnotherContract.java
+++ support-api/src/main/java/com/zte/crm/prm/supportapi/AnotherContract.java
@@ -1,9 +1,13 @@
 package com.zte.crm.prm.supportapi;
 
 import com.zte.crm.prm.anno.RemoteServiceContract;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RemoteServiceContract(name = "zte-crm-prm-common",url = "http://localhost:8881")
+@FeignClient(name = "zte-crm-prm-common",
+        url = "http://localhost:8881"
+    )
+@RemoteServiceContract
 public interface AnotherContract {
 
     @RequestMapping("/welcome")
--- support-api/src/main/java/com/zte/crm/prm/supportapi/HelloContract.java
+++ support-api/src/main/java/com/zte/crm/prm/supportapi/HelloContract.java
@@ -1,14 +1,18 @@
 package com.zte.crm.prm.supportapi;
 
 import com.zte.crm.prm.anno.RemoteServiceContract;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
-@RemoteServiceContract(name = "zte-crm-prm-common",
-
-        url = "http://localhost:8881"
-)
+@FeignClient(name = "zte-crm-prm-common",
+        url = "http://localhost:8881",
+        contextId = "feignclient#25a8dd6bd775732aae7efe76e1300b83")
+@RemoteServiceContract
+@RequestMapping("/fc/fc/fc")
 public interface HelloContract {
+
+
+
     @RequestMapping("/hello")
     String hello();
 }
